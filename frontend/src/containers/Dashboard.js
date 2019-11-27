f8db2e34394f79d42545a3b5cd3f403defd8a60f
Thomas <thomas.lemercier@edu.ece.fr>
2019-11-27 14:14:03 +0100

    changement booststrap

--- frontend/src/containers/Dashboard.js
+++ frontend/src/containers/Dashboard.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import PieChart from '../components/PieChart/PieChart.js';
+import Card from 'react-bootstrap/Card'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 class Dashboard extends React.Component {
   constructor(props) {
@@ -11,10 +15,19 @@ class Dashboard extends React.Component {
 
   render() {
     return (
-      <div className="">
-        <PieChart></PieChart>
-      </div>
-    );
+      <Container>
+        <Card className="text-center d-flex flex-center">
+          <Card.Header as="h5">Featured</Card.Header>
+          <Card.Body>
+            <Card.Title>PieChart</Card.Title>
+            <Card.Text>
+              With supporting text below as a natural lead-in to additional content.
+            </Card.Text>
+            <PieChart></PieChart>
+          </Card.Body>
+        </Card>
+      </Container>
+      );
   }
 }
 
