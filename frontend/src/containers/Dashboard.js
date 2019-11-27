85c02adc4661e553fa3a443bfbf617f8be415af7
Tom Jouvet <tom.jouvet@live.fr>
2019-11-27 14:39:14 +0100

    Add AreaChart

--- frontend/src/containers/Dashboard.js
+++ frontend/src/containers/Dashboard.js
@@ -1,9 +1,5 @@
 import React from 'react';
-import PieChart from '../components/PieChart/PieChart.js';
-import Card from 'react-bootstrap/Card'
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
+import Widgets from './Widgets';
 
 class Dashboard extends React.Component {
   constructor(props) {
@@ -15,18 +11,7 @@ class Dashboard extends React.Component {
 
   render() {
     return (
-      <Container>
-        <Card className="text-center d-flex flex-center">
-          <Card.Header as="h5">Featured</Card.Header>
-          <Card.Body>
-            <Card.Title>PieChart</Card.Title>
-            <Card.Text>
-              With supporting text below as a natural lead-in to additional content.
-            </Card.Text>
-            <PieChart></PieChart>
-          </Card.Body>
-        </Card>
-      </Container>
+      <Widgets></Widgets>
       );
   }
 }
