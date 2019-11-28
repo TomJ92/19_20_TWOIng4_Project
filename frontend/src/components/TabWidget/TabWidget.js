eeafaabe4b3441a72522693b74e6154a8b9485c2
Thomas <thomas.lemercier@edu.ece.fr>
2019-11-28 19:59:10 +0100

    2 3 modifs minimes

--- frontend/src/components/TabWidget/TabWidget.js
+++ frontend/src/components/TabWidget/TabWidget.js
@@ -20,6 +20,7 @@ export default class TabWidget extends React.Component {
                 <th>Handle</th>
               </tr>
             </MDBTableHead>
+
             <MDBTableBody>
               <tr>
                 <td>1</td>
@@ -27,12 +28,14 @@ export default class TabWidget extends React.Component {
                 <td>Otto</td>
                 <td>@mdo</td>
               </tr>
+
               <tr>
                 <td>2</td>
                 <td>Jacob</td>
                 <td>Thornton</td>
                 <td>@fat</td>
               </tr>
+
               <tr>
                 <td>3</td>
                 <td>Larry</td>
