42ba13d8b36c9d274acf14ba58608c66d324e79f
Tom Jouvet <tom.jouvet@live.fr>
2019-12-10 23:27:25 +0100

    Are Chart COMPLETED

--- backend/DashboardAPI/routes/measures.js
+++ backend/DashboardAPI/routes/measures.js
@@ -28,6 +28,61 @@ router.get('/count_pollution', Measure.count_pollution);
 
 router.get('/list_id', Measure.list_ID);
 
+// GET Number of instances per type
+
+router.get('/humidity_january', Measure.humidity_january);
+// GET Number of instances per type
+
+router.get('/humidity_february', Measure.humidity_february);
+// GET Number of instances per type
+
+router.get('/humidity_march', Measure.humidity_march);
+// GET Number of instances per type
+
+router.get('/humidity_april', Measure.humidity_april);
+// GET Number of instances per type
+
+router.get('/humidity_may', Measure.humidity_may);
+// GET Number of instances per type
+
+router.get('/humidity_june', Measure.humidity_june);
+// GET Number of instances per type
+
+router.get('/temperature_january', Measure.temperature_january);
+// GET Number of instances per type
+
+router.get('/temperature_february', Measure.temperature_february);
+// GET Number of instances per type
+
+router.get('/temperature_march', Measure.temperature_march);
+// GET Number of instances per type
+
+router.get('/temperature_april', Measure.temperature_april);
+// GET Number of instances per type
+
+router.get('/temperature_may', Measure.temperature_may);
+// GET Number of instances per type
+
+router.get('/temperature_june', Measure.temperature_june);
+// GET Number of instances per type
+
+router.get('/pollution_january', Measure.pollution_january);
+// GET Number of instances per type
+
+router.get('/pollution_february', Measure.pollution_february);
+// GET Number of instances per type
+
+router.get('/pollution_march', Measure.pollution_march);
+// GET Number of instances per type
+
+router.get('/pollution_april', Measure.pollution_april);
+// GET Number of instances per type
+
+router.get('/pollution_may', Measure.pollution_may);
+// GET Number of instances per type
+
+router.get('/pollution_june', Measure.pollution_june);
+
 
 
 
